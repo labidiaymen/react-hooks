c346a1dff5569139ca4cf86641817f65f1f900d6
Aymen <labidiaymen@outlook.com>
2018-10-31 15:09:37 +0100

    init app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function useFormInput(init_value) {
+  const [value, setValue] = useState(init_value);
+  function handleChange(e) {
+    setValue(e.target.value);
+  }
+  return {
+    value,
+    onChange: handleChange
+  }
+}
+
+function LoginForm() {
+  const name = useFormInput('');
+  const lastName = useFormInput('');
+  return (
+    <div>
+      <div className="form-control">
+        <label> Name : </label>
+        <input {...name} />
+      </div>
+      <div className="form-control">
+        <label> Lastname : </label>
+        <input {...lastName} />
+      </div>
+      <div>
+        {name.value}  {lastName.value}
+      </div>
+    </div>
+  );
+}
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header className="">
+          <LoginForm />
         </header>
       </div>
     );
